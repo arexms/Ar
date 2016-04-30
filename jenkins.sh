78b25824e453ed1189e269adf3c73f936738a556
arexms <arex.ms@gmail.com>
2016-04-30 19:20:30 +0200

    Introduce Jenkis' rule to execute make clean

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 cd build
 cmake .
+echo $1
+if [ "$1" == true ]
+then
+    echo "Cleaning workspace..."
+    make clean
+fi
+echo "Building..."
 make -j
+echo "Running tests..."
 make runTests
 cd ..
