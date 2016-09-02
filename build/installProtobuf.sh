4b0a2c504c6281adc72776bfbbe27549d79d9e0c
arexms <arex.ms@gmail.com>
2016-09-03 00:31:39 +0200

    travis config - fix #30 generateProtobuf update

--- build/installProtobuf.sh
+++ build/installProtobuf.sh
@@ -1,9 +1,5 @@
-if [ -d '$HOME/protobuf-3.0.0-bin' ]; then
+if [ -d "$HOME/protobuf-3.0.0-bin" ]; then
     echo "protobuf already installed"
 else
     echo "Downloading protobuf..."
-    wget https://github.com/google/protobuf/releases/download/v3.0.0/protobuf-cpp-3.0.0.tar.gz
-    tar xvfz protobuf-cpp-3.0.0.tar.gz
-    rm protobuf-cpp-3.0.0.tar.gz
-    ( cd protobuf-3.0.0 && ./configure --prefix=$HOME/protobuf-3.0.0-bin && make -j2 && make install && cd .. )
 fi
