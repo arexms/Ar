100e9395eeba7bc1144b8be6a625fd64e5d2bb58
arexms <arex.ms@gmail.com>
2016-09-03 00:14:04 +0200

    travis config - fix #26 generateProtobuf update

--- build/generateProtobuf.sh
+++ build/generateProtobuf.sh
@@ -6,8 +6,8 @@ else
     echo "Generating protobuf...";
     protoc -I../includes/Ar/Raspi/Messages/ --cpp_out=. ../includes/Ar/Raspi/Messages/RaspiMessages.proto;
     mv RaspiMessages.pb.h ../includes/Ar/Raspi/Messages/
-    sed -i "\"1i/// @date `date`\"" RaspiMessages.pb.cc
-    sed -i "'s|include \"RaspiMessages.pb.h\"|include <Ar/Raspi/Messages/RaspiMessages.pb.h>|g'" RaspiMessages.pb.cc
+    sed -i "1i/// @date `date`" RaspiMessages.pb.cc
+    sed -i 's|include \"RaspiMessages.pb.h\"|include <Ar/Raspi/Messages/RaspiMessages.pb.h>|g' RaspiMessages.pb.cc
     mv RaspiMessages.pb.cc ../sources/Ar/Raspi/Messages/RaspiMessages.pb.cpp
     md5sum ../includes/Ar/Raspi/Messages/RaspiMessages.proto > protobuf.md5
 fi
