6cad880220aa1497901f6f181a4592aefeb23ce0
arexms <arex.ms@gmail.com>
2016-09-05 21:23:38 +0200

    Work on coveralls report

--- build/generateAndSubmitCoverallsReport.sh
+++ build/generateAndSubmitCoverallsReport.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+echo "Capturing..."
 lcov --directory . --capture --output-file coverage.info
+echo "Removing unnecessary files..."
 lcov --remove coverage.info 'tests/*' '/usr/include/*' 'protobuf-3.0.0-bin/*' 'googletest/*' 'googlemock/*' --output-file coverage.info
+echo "Listing..."
 lcov --list coverage.info
-coveralls-lcov --repo-token $COVERALLS_TOKEN coverage.info
+echo "Submitting..."
+#coveralls-lcov --repo-token $COVERALLS_TOKEN coverage.info
